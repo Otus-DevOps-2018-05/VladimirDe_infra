4c085aa639884763f1b929ed81c3ebcda86283ab
Vladimir Denisov <vladimirdenisov69@gmail.com>
2018-08-05 22:11:33 +0300

    Path to files in packer json

--- play-travis/packer-validate.sh
+++ play-travis/packer-validate.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 for TEMPLATE in packer/*.json;do
+    echo  "Validate packer template ${template}"
     docker run -i  -t --volume $(pwd):/data --workdir /data packer-ansible validate -var-file=packer/variables.json.example ${TEMPLATE}
 done
